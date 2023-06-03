130a263c5c8ba133601d3007e13965e0ed712314
Tyler Herron <tylerherron7777@gmail.com>
2023-06-02 22:48:20 -0500

    audio and playlists selection

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-from flask import Flask, render_template
+import music.database
+from flask import Flask, render_template, request, jsonify, json
 app = Flask(__name__)
 
-
-
-import music.database
-playlist_data=music.database.connect("localhost","Tyler","Blackrobin7","music","SELECT * FROM Slime")
-output_playlist=[]
+current_playlist="Slime"
+playlist_data = music.database.connect(
+    "localhost", "musicsite", "MGZGvCFtmGN*1OUM", "music", f"SELECT * FROM {current_playlist}")
+output_playlist = []
 for song in playlist_data:
     output_playlist.append(song)
 
-
-
-
+text = str(output_playlist)
+converted_text = text.replace("'", "\"")
+#
+playlist_choices= music.database.connect(
+    "localhost", "musicsite", "MGZGvCFtmGN*1OUM", "music", "SHOW TABLES")
+print(playlist_choices[0])
+final=""
+for temp in playlist_choices:
+    for text in temp:
+        final+=" "+text
 @app.route('/')
 def display_songs():
-    
-    return render_template('index.html', songs=output_playlist)
+    info = converted_text
+
+    return render_template('index.html', songs=output_playlist,info=info,choices=final)
+
 
 if __name__ == '__main__':
     app.run()
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,25 +1,62 @@
 const tracksList = document.getElementById("playlist-tracks");
-const tracks = tracksList.getElementsByTagName("li");const total = document.getElementById("total-songs");
+const tracks = tracksList.getElementsByTagName("li"); const total = document.getElementById("total-songs");
 const playButtonDiv = document.getElementById("play-button-div"); const pauseButtonDiv = document.getElementById("pause-button-div");
 const playButton = document.getElementById("play-button"); const pauseButton = document.getElementById("pause-button");
-
+const audio = document.getElementById("audio");
+let i = 0
+let shuffle = false;
 playButton.addEventListener("click", () => {
-pauseButtonDiv.classList.remove("clear")
-playButtonDiv.classList.add("clear")
+    pauseButtonDiv.classList.remove("clear")
+    playButtonDiv.classList.add("clear")
+    audio.play()
 
 });
 pauseButton.addEventListener("click", () => {
-playButtonDiv.classList.remove("clear")
-pauseButtonDiv.classList.add("clear")
+    playButtonDiv.classList.remove("clear")
+    pauseButtonDiv.classList.add("clear")
+    audio.pause()
 
 });
 const shuffleIcon = document.getElementById("shuffle-icon");
-const shuffleButton=document.getElementById("shuffle-button");
+const shuffleButton = document.getElementById("shuffle-button");
 shuffleButton.addEventListener("click", () => {
-if (shuffleIcon.style.color != "red") 
-{ shuffleIcon.style.color = "red" } 
-else 
-{ shuffleIcon.style.color = 'black' }
+    if (shuffleIcon.style.color != "red") {
+        shuffleIcon.style.color = "red";
+        shuffle = true;
+    }
+    else {
+        shuffleIcon.style.color = 'black';
+        shuffle = false;
+    }
 
 
 })
+
+function next() {
+    if (shuffle = true) {
+        let randIndex = Math.floor(Math.random() * playlistData.length);
+        i = randIndex
+        audio.src = links[i]
+
+    } else {
+
+
+        i++
+        audio.src = links[i]
+    }
+}
+audio.onended = () => {
+    next();
+};
+
+audio.addEventListener('loadedmetadata', function () {
+    var duration = audio.duration;
+    console.log(duration)
+    var temp = duration / 60
+    const roundedNumber = temp.toFixed(2);
+    
+    console.log(roundedNumber);
+    
+
+    document.getElementById("length").innerHTML = "Length:" + roundedNumber;
+});
