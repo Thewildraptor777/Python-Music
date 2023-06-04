471f5cbc977179f6b73c6e5d77759f05b5021a49
Tyler Herron <tylerherron7777@gmail.com>
2023-06-03 21:52:11 -0500

    migrating data

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
 import music.database
 from flask import Flask, render_template, request, jsonify, json
+from apscheduler.schedulers.background import BackgroundScheduler
+
 app = Flask(__name__)
 
+current_playlist = "Slime"
 
+def update_playlist():
+    global output_playlist, converted_text
+    playlist_data = music.database.connect(
+        "localhost", "musicsite", "MGZGvCFtmGN*1OUM", "music", f"SELECT * FROM {current_playlist}")
+    output_playlist = []
+    for song in playlist_data:
+        output_playlist.append(song)
 
-current_playlist="Slime"
+    text = str(output_playlist)
+    converted_text = text.replace("'", "\"")
 
-playlist_data = music.database.connect(
-    "localhost", "musicsite", "MGZGvCFtmGN*1OUM", "music", f"SELECT * FROM {current_playlist}")
-output_playlist = []
-for song in playlist_data:
-    output_playlist.append(song)
-
-text = str(output_playlist)
-converted_text = text.replace("'", "\"")
-#
-playlist_choices= music.database.connect(
+scheduler = BackgroundScheduler()
+scheduler.add_job(update_playlist, 'interval', seconds=3)
+scheduler.start()
+playlist_choices = music.database.connect(
     "localhost", "musicsite", "MGZGvCFtmGN*1OUM", "music", "SHOW TABLES")
-print(playlist_choices[0])
-final=""
-for temp in playlist_choices:
-    for text in temp:
-        final+=" "+text
+final = ""
+for setup in playlist_choices:
+    for text in setup:
+        final += " " + text
+
 @app.route('/')
 def display_site():
     info = converted_text
-
-    return render_template('index.html', songs=output_playlist,info=info,choices=final)
-
+    return render_template('index.html', songs=output_playlist, info=info, choices=final)
 
 if __name__ == '__main__':
     app.run()
