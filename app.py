542db6ef0e35a0347655d4dc462e7bac8a316441
Tyler Herron <tylerherron7777@gmail.com>
2023-06-03 21:57:18 -0500

    ajax

--- app.py
+++ app.py
@@ -1,36 +1,16 @@
-import music.database
-from flask import Flask, render_template, request, jsonify, json
-from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-current_playlist = "Slime"
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-def update_playlist():
-    global output_playlist, converted_text
-    playlist_data = music.database.connect(
-        "localhost", "musicsite", "MGZGvCFtmGN*1OUM", "music", f"SELECT * FROM {current_playlist}")
-    output_playlist = []
-    for song in playlist_data:
-        output_playlist.append(song)
+@app.route("/process_variable")
+def process_variable():
+    variable = request.args.get("variable")
+    response = f"You sent: {variable}"
+    return response
 
-    text = str(output_playlist)
-    converted_text = text.replace("'", "\"")
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(update_playlist, 'interval', seconds=3)
-scheduler.start()
-playlist_choices = music.database.connect(
-    "localhost", "musicsite", "MGZGvCFtmGN*1OUM", "music", "SHOW TABLES")
-final = ""
-for setup in playlist_choices:
-    for text in setup:
-        final += " " + text
-
-@app.route('/')
-def display_site():
-    info = converted_text
-    return render_template('index.html', songs=output_playlist, info=info, choices=final)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- static/scripts/tracks.js
+++
@@ -1,7 +0,0 @@
-
-let playlistTracks = document.getElementsByTagName('li');
-console.log(playlistTracks)
-
-for(i=0;i<playlistTracks.length;i++){
-   
-}
