756012b15fd3be96cc9bf2b154d4cd7ac6b34fa1
Tyler Herron <tylerherron7777@gmail.com>
2023-06-04 23:09:04 -0500

    upload file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from music import database
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, send_file,url_for,redirect
+import pygame
+import os
 app = Flask(__name__)
 
 current_playlist = "Slime"
@@ -8,6 +9,7 @@ output_playlist = []  # Initialize the variable here
 converted_text = ""
 stored_variable = ""  # Define stored_variable here
 stored_variable = current_playlist
+
 
 def update_playlist():
     global output_playlist, converted_text
@@ -21,20 +23,24 @@ def update_playlist():
     converted_text = text.replace("'", "\"")
     print(stored_variable)
 
+
 # Initialize the output_playlist variable before starting the application
 update_playlist()
 
-playlist_choices= database.connect(
+playlist_choices = database.connect(
     "localhost", "musicsite", "MGZGvCFtmGN*1OUM", "music", "SHOW TABLES")
-#print(playlist_choices[0])
-final=""
+# print(playlist_choices[0])
+final = ""
 for temp in playlist_choices:
     for text in temp:
-        final+=text+"  "
+        final += text+"  "
+
+
 @app.route('/')
 def display_site():
     info = converted_text
-    return render_template('index.html', songs=output_playlist, info=info,choices=final)
+    return render_template('index.html', songs=output_playlist, info=info, choices=final)
+
 
 @app.route("/process_variable")
 def process_variable():
@@ -46,10 +52,65 @@ def process_variable():
     response = f"You sent: {variable}"
     return response
 
+
 @app.route("/retrieve_variable")
 def retrieve_variable():
     global stored_variable
     return f"The stored variable is: {stored_variable}"
 
+# Function to play audio file
+def play_audio(file_path):
+    pygame.mixer.init()
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+
+# Folder containing audio files
+folder_path = 'audio'
+
+@app.route('/audio/')
+def index():
+    audio_files = []
+
+    # Iterate over the files in the folder
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.mp3') or filename.endswith('.wav'):
+            audio_files.append(filename)
+
+    return render_template('audio.html', audio_files=audio_files)
+
+@app.route('/audio/play/<filename>')
+def play(filename):
+    file_path = os.path.join(folder_path, filename)
+    play_audio(file_path)
+
+
+
+# Set the upload folder path
+UPLOAD_FOLDER = 'audio/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        # Check if a file is included in the request
+        if 'audio_file' not in request.files:
+            return redirect(request.url)
+
+        audio_file = request.files['audio_file']
+
+        # Check if the file is empty
+        if audio_file.filename == '':
+            return redirect(request.url)
+
+        # Save the file to the upload folder
+        audio_file.save(os.path.join(app.config['UPLOAD_FOLDER'], audio_file.filename))
+
+        return 'File uploaded successfully!'
+
+    return render_template('submit.html')
+
+
+    return f"Playing: {filename}"
 if __name__ == '__main__':
     app.run()
